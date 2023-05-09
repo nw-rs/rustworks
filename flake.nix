d35636a8b66f7ce629d3140fcdfa8024b28f3005
willemml <willem@leit.so>
2023-05-09 12:25:52 -0700

    add nix flake for arm toolchain, general update

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A simple flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [fenix.overlays.default];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = let
+            arm-gcc =
+              if system == "aarch64-darwin"
+              then (pkgs // {system = "x86_64-darwin";}).gcc-arm-embedded
+              else pkgs.gcc-arm-embedded;
+          in
+            with pkgs; [
+              arm-gcc
+              (fenix.packages.${system}.combine (with fenix.packages.${system}; [
+                latest.toolchain
+                targets.thumbv7em-none-eabihf.latest.rust-std
+              ]))
+            ];
+        };
+      }
+    );
+}
